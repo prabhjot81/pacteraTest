bb1b6febc7593e423f22dde6181393d834c087f5
Prabhjot Singh <prabhjot81@hotmail.com>
2015-07-26 19:05:38 +1000

    Initial Commit

---
+++ Pactera Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pactera Test
+//
+//  Created by Prabhjot Singh on 26/07/2015.
+//  Copyright (c) 2015 Pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pactera Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pactera Test
+//
+//  Created by Prabhjot Singh on 26/07/2015.
+//  Copyright (c) 2015 Pactera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
